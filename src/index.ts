1547b3631c66c9056644aa1300b5feddd70e8147
Anderson Pavani <anderson.pavani@gmail.com>
2023-10-06 17:35:21 -0400

    Exemplo finalizado

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,29 @@
-import { Elysia } from "elysia";
+import app from "./external/api/config";
+import RegistrarUsuario from "./core/usuario/service/RegistrarUsuario";
+import RegistrarUsuarioController from "./adapters/RegistrarUsuarioController";
+import RepositorioUsuarioPrismaPg from "./external/prisma/RepositorioUsuarioPrismaPg";
+import ConsultarUsuarios from "./core/usuario/service/ConsultarUsuarios";
+import ConsultarUsuariosController from "./adapters/ConsultarUsuariosController";
+import ConsultarUsuarioPorId from "./core/usuario/service/ConsultarUsuarioPorId";
+import ConsultarUsuarioPorIdController from "./adapters/ConsultarUsuarioPorIdController";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+
+app.get("/", () => "API Rest Cadastro de Usuários");
+
+//Registrar Rotas
+const repositorioUsuario = new RepositorioUsuarioPrismaPg();
+
+const registrarUsuario = new RegistrarUsuario(repositorioUsuario);
+new RegistrarUsuarioController(app, registrarUsuario);
+
+const consultarUsuarios = new ConsultarUsuarios(repositorioUsuario);
+new ConsultarUsuariosController(app, consultarUsuarios);
+
+const consultarUsuarioPorId = new ConsultarUsuarioPorId(repositorioUsuario);
+new ConsultarUsuarioPorIdController(app, consultarUsuarioPorId);
+
+
+app.listen(3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
